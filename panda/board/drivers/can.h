0ec5b80747c34d67254a6def87763f62959ba4dd
Jason Shuler <jshuler@gmail.com>
2019-11-24 15:04:47 -0500

    Attempted bolt ignition workaround

--- panda/board/drivers/can.h
+++ panda/board/drivers/can.h
@@ -343,25 +343,25 @@ void ignition_can_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
   if (bus == 0) {
     // GM exception
-    if ((addr == 0x1F1)) {
+    if ((addr == 0x1F1) && (len == 8)) {
       //Bit 5 is ignition "on"
       ignition_can = (GET_BYTE(to_push, 0) & 0x20) != 0;
     }
-    // Tesla exception
-    if ((addr == 0x348) && (len == 8)) {
-      // GTW_status
-      ignition_can = (GET_BYTE(to_push, 0) & 0x1) != 0;
-    }
-    // Cadillac exception
-    if ((addr == 0x160) && (len == 5)) {
-      // this message isn't all zeros when ignition is on
-      ignition_can = GET_BYTES_04(to_push) != 0;
-    }
-    // VW exception
-    if ((addr == 0x3C0) && (len == 4)) {
-     // VW Terminal 15 (ignition-on) state
-     ignition_can  = (GET_BYTE(to_push, 2) & 0x2) != 0;
-    }
+    // // Tesla exception
+    // if ((addr == 0x348) && (len == 8)) {
+    //   // GTW_status
+    //   ignition_can = (GET_BYTE(to_push, 0) & 0x1) != 0;
+    // }
+    // // Cadillac exception
+    // if ((addr == 0x160) && (len == 5)) {
+    //   // this message isn't all zeros when ignition is on
+    //   ignition_can = GET_BYTES_04(to_push) != 0;
+    // }
+    // // VW exception
+    // if ((addr == 0x3C0) && (len == 4)) {
+    //  // VW Terminal 15 (ignition-on) state
+    //  ignition_can  = (GET_BYTE(to_push, 2) & 0x2) != 0;
+    // }
   }
 }
 
